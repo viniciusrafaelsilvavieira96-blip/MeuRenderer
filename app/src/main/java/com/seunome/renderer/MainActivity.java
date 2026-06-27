8a07f09348515dd4cf76dda2b0f704b509ec6681
SeuNome <seuemail@gmail.com>
2026-06-27 20:04:04 -0300

    clean repo

---
+++ app/src/main/java/com/seunome/renderer/MainActivity.java
@@ -0,0 +1,11 @@
+package com.seunome.renderer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
